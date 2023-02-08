c4db76991316f9b1faed2dc13c72b5016c323673
Pedro Alex <pedroalex663@gmail.com>
2023-02-08 19:56:58 -0300

    fifth challenge

---
+++ calculo simples/script.js
@@ -0,0 +1,20 @@
+
+let codidoDaPeca = 12
+let numeroDePecas = 1
+let valorUnitario = 5.30
+
+let valorASerPagoNaPrimeiraPeca = valorUnitario *numeroDePecas
+
+
+
+let codigoDaPecaTwo = 16
+let numeroDePecasTwo = 2
+let valorUnitarioTwo = 5.10
+
+let valorASerPagoNaSegundaPeca = valorUnitarioTwo *numeroDePecasTwo
+
+
+let valorASerPago = valorASerPagoNaPrimeiraPeca + valorASerPagoNaSegundaPeca
+
+console.log("VALOR A PAGAR: R$ " + valorASerPago.toFixed(2))
+
