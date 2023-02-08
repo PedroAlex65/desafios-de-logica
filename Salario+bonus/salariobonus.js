2df220677cafea0a05c4dc0f76c4cbca1d870c75
Pedro Alex <pedroalex663@gmail.com>
2023-02-08 14:25:33 -0300

    third challenge

---
+++ Salario+bonus/salariobonus.js
@@ -0,0 +1,11 @@
+
+
+let seller = 'JOAO'
+
+let vendas = 1230.30
+
+let salaryFixo = 500
+
+let salaryToBeBeceived = vendas * (15/100) + salaryFixo
+
+console.log("TOTAL = R$ " + salaryToBeBeceived.toFixed(2))
